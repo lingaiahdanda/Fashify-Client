7783de5dc9b551e5dca5a02953120ac8228fb584
lingaiahdanda <lingaiahdanda@gmail.com>
2023-02-11 21:00:47 -0600

    Added Product detailing page and Login/Regsiter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Home from "./pages/Home";
 import "./App.css"
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = ()=>{
   return (
